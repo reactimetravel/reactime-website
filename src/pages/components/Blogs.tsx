5d87ef516e4316cef14423776bf88cda1fdaee95
KyEBell <kyleevanbell@gmail.com>
2023-08-09 20:49:49 -0400

    updated Blog post image

--- src/pages/components/Blogs.tsx
+++ src/pages/components/Blogs.tsx
@@ -18,7 +18,7 @@ const posts = [
     author: {
       name: "Sean Kelly",
       href: "https://medium.com/@brok3turtl3",
-      imageUrl: "https://i.imgur.com/68CnpzY.jpg",
+      imageUrl: "https://images.unsplash.com/photo-1600267185393-e158a98703de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
     },
   },
   {
