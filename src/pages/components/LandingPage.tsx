7d302ec14abc3e3a04b329e492f05055fdc0b2d9
dev-plam <peterlamx@gmail.com>
2023-10-20 16:54:16 -0700

    updated p element in landing page

--- src/pages/components/LandingPage.tsx
+++ src/pages/components/LandingPage.tsx
@@ -62,7 +62,8 @@ export default function LandingPage() {
                       <p className="mt-6 text-xl text-gray-500">
                         Download the Google Chrome Extension that rewinds time
                         and replays previous versions of stateful React
-                        components. Now compatible with Next.js and Remix.
+                        components. Now compatible with Next.js and Remix
+                        applications.
                       </p>
                       {/* This code is for the newsletter */}
                       {/* <p className="className=mt-6 mt-10 text-xl text-gray-500">
