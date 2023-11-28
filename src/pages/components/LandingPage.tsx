f4198bda587993a4cc39657126a364106e077fc8
dev-plam <peterlamx@gmail.com>
2023-11-27 23:42:45 -0800

    added asterisk for landing page info

--- src/pages/components/LandingPage.tsx
+++ src/pages/components/LandingPage.tsx
@@ -63,7 +63,7 @@ export default function LandingPage() {
                         Download the Google Chrome Extension that rewinds time
                         and replays previous versions of stateful React
                         components. Now compatible with Next.js and Remix
-                        applications.
+                        applications*.
                       </p>
                       {/* This code is for the newsletter */}
                       {/* <p className="className=mt-6 mt-10 text-xl text-gray-500">
